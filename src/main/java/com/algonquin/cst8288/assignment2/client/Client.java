2b62a01b3a2ffdb7ad7fb483651b96fe7d096498
Andres Porras <camilop1730@gmail.com>
2024-02-15 21:59:35 -0500

    Database Connection successful with MYSQL DB

--- src/main/java/com/algonquin/cst8288/assignment2/client/Client.java
+++ src/main/java/com/algonquin/cst8288/assignment2/client/Client.java
@@ -1,8 +1,16 @@
 package com.algonquin.cst8288.assignment2.client;
+import com.algonquin.cst8288.assignment2.database.DBConnection;
+import java.sql.SQLException;
+
 
 public class Client {
 	
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws SQLException {
+            //Singleton
+            DBConnection dbcon = new DBConnection();
+            dbcon.stablishConnection();
+             DBConnection dbcon2 = new DBConnection();
+             dbcon2.stablishConnection();
+           
 	}
 }
--- src/main/java/com/algonquin/cst8288/assignment2/database/DBConnection.java
+++ src/main/java/com/algonquin/cst8288/assignment2/database/DBConnection.java
@@ -8,19 +8,27 @@ import java.sql.SQLException;
 
 public class DBConnection {
 	
-	private static Connection connection = null;
-        PreparedStatement prepStatement = null; // prepares query for execution
-        ResultSet resultSet = null; // manages results
-	private String serverUrl = "jdbc:mysql://localhost:3306/bookvault";
-	private String userString = "root";
-	private String passwordString = "AlgoFacil123#";
-	private String driverString = "com.mysql.cj.jdbc.Driver";
-        
-        public void stablishConnection() throws SQLException{
+
+    private static Connection connection = null;
+    PreparedStatement prepStatement = null; // prepares query for execution
+    ResultSet resultSet = null; // manages results
+    private String serverUrl = "jdbc:mysql://localhost:3306/bookvault";
+    private String userString = "root";
+    private String passwordString = "AlgoFacil123#";
+    private String driverString = "com.mysql.cj.jdbc.Driver";
+
+    public void stablishConnection() throws SQLException{
+        try {
+        if (connection==null){
             connection = DriverManager.getConnection(serverUrl, userString, passwordString);
-            
+            System.out.println("Connection successful");
+        } else System.out.println("Using existing connection");
+        } catch (SQLException ex) {
+            ex.printStackTrace();
         }
-        
+
+    }
+
         
 	
 
